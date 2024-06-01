4cd9f0cf970381cbd010547da2aa3048083bb14b
blockchainaddict <vazquezbare@gmail.com>
2024-06-01 18:38:40 +0200

    route correction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router'
-import { AppComponent } from './app.component';
+import { CountdownComponent } from './countdown/countdown.component';
 
 export const routes: Routes = [
-  { path: 'home', loadComponent: () => import('./countdown/countdown.component').then(m => m.CountdownComponent) },
+  { path: '', component: CountdownComponent }, 
   { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent) },
 ];
